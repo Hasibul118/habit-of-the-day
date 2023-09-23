95d1ce7ae997059dec348c954e47c7330240c55d
Hasibul Hasan <hasibshanto85@gmail.com>
2023-09-24 02:07:15 +0600

    v1.0.1 > bug fix

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -1,21 +1,42 @@
-import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 import PlayButton from "./PlayButton";
 import PauseButton from "./PauseButton";
 import SettingsButton from "./SettingsButton";
-import {useContext, useState, useEffect, useRef} from "react";
+import { useContext, useState, useEffect, useRef } from "react";
 import SettingsContext from "./SettingsContext";
 
-import Icon from '/favicon.ico'; 
+import Icon from "/favicon.ico";
 
-const red = 'rgb(255, 0, 0, 0.9)';
-const green = '#4aec8c';
+const red = "rgb(255, 0, 0, 0.9)";
+const green = "#4aec8c";
+
+const showNotification = () => {
+  // Play a sound
+  const audio = new Audio("/notification.mp3");
+  audio.play();
+
+  if (Notification.permission === "granted") {
+    const notificationOptions = {
+      icon: Icon,
+    };
+
+    new Notification("Your timer has ended!", notificationOptions);
+    
+  } else if (Notification.permission !== "denied") {
+    Notification.requestPermission().then((permission) => {
+      if (permission === "granted") {
+        showNotification();
+      }
+    });
+  }
+};
 
 function Timer() {
   const settingsInfo = useContext(SettingsContext);
 
   const [isPaused, setIsPaused] = useState(true);
-  const [mode, setMode] = useState('work'); // work/break/null
+  const [mode, setMode] = useState("work"); // work/break/null
   const [secondsLeft, setSecondsLeft] = useState(0);
 
   const secondsLeftRef = useRef(secondsLeft);
@@ -27,30 +48,7 @@ function Timer() {
     setSecondsLeft(secondsLeftRef.current);
   }
 
-  const showNotification = () => {
-    // Play a sound
-    const audio = new Audio('/notification.mp3');
-    audio.play();
-  
-    if (Notification.permission === 'granted') {
-      const notificationOptions = {
-        icon: Icon,
-      };
-  
-      new Notification('Your timer has ended!', notificationOptions);
-  
-    } else if (Notification.permission !== 'denied') {
-      Notification.requestPermission().then((permission) => {
-        if (permission === 'granted') {
-          showNotification();
-        }
-      });
-    }
-  };
-  
-
   useEffect(() => {
-
     if (!("Notification" in window)) {
       console.log("Browser does not support desktop notification");
     } else {
@@ -58,10 +56,13 @@ function Timer() {
     }
 
     function switchMode() {
-      const nextMode = modeRef.current === 'work' ? 'break' : 'work';
-      const nextSeconds = (nextMode === 'work' ? settingsInfo.workMinutes : settingsInfo.breakMinutes) * 60;
+      const nextMode = modeRef.current === "work" ? "break" : "work";
+      const nextSeconds =
+        (nextMode === "work"
+          ? settingsInfo.workMinutes
+          : settingsInfo.breakMinutes) * 60;
 
-      setMode(nextMode)
+      setMode(nextMode);
       modeRef.current = nextMode;
 
       setSecondsLeft(nextSeconds);
@@ -82,38 +83,54 @@ function Timer() {
       }
 
       tick();
-    },1000);
+    }, 1000);
 
     return () => clearInterval(interval);
   }, [settingsInfo]);
 
-  const totalSeconds = mode === 'work'
-    ? settingsInfo.workMinutes * 60
-    : settingsInfo.breakMinutes * 60;
-  const percentage = Math.round(secondsLeft / totalSeconds * 100);
+  const totalSeconds =
+    mode === "work"
+      ? settingsInfo.workMinutes * 60
+      : settingsInfo.breakMinutes * 60;
+  const percentage = Math.round((secondsLeft / totalSeconds) * 100);
 
   const minutes = Math.floor(secondsLeft / 60);
   let seconds = secondsLeft % 60;
-  if(seconds < 10) seconds = '0'+seconds;
+  if (seconds < 10) seconds = "0" + seconds;
 
   return (
     <div>
-      <h1 style={{marginBottom: '0', paddingTop: '30px'}}>Pomodoro Timer</h1>
-      <p style={{marginTop: '5px', marginBottom: '40px', fontSize:'16px'}}>Attaining your utmost productivity</p>
+      <h1 style={{ marginBottom: "0", paddingTop: "30px" }}>Pomodoro Timer</h1>
+      <p style={{ marginTop: "5px", marginBottom: "40px", fontSize: "16px" }}>
+        Attaining your utmost productivity
+      </p>
       <CircularProgressbar
         value={percentage}
-        text={minutes + ':' + seconds}
+        text={minutes + ":" + seconds}
         styles={buildStyles({
-        textColor:'#fff',
-        pathColor:mode === 'work' ? red : green,
-        tailColor:'rgba(255,255,255,.2)',
-      })} />
-      <div style={{marginTop:'20px'}}>
-        {isPaused
-          ? <PlayButton onClick={() => { setIsPaused(false); isPausedRef.current = false; }} />
-          : <PauseButton onClick={() => { setIsPaused(true); isPausedRef.current = true; }} />}
+          textColor: "#fff",
+          pathColor: mode === "work" ? red : green,
+          tailColor: "rgba(255,255,255,.2)",
+        })}
+      />
+      <div style={{ marginTop: "20px" }}>
+        {isPaused ? (
+          <PlayButton
+            onClick={() => {
+              setIsPaused(false);
+              isPausedRef.current = false;
+            }}
+          />
+        ) : (
+          <PauseButton
+            onClick={() => {
+              setIsPaused(true);
+              isPausedRef.current = true;
+            }}
+          />
+        )}
       </div>
-      <div style={{marginTop:'20px'}}>
+      <div style={{ marginTop: "20px" }}>
         <SettingsButton onClick={() => settingsInfo.setShowSettings(true)} />
       </div>
     </div>
